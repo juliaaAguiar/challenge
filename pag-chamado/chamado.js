0247b9d188feb688de1b7aa6706442d4084011bd
pedroarthuralvesdeoliveira <pedroarthuralvesdeoliveira@hotmail.com>
2024-05-02 23:15:03 -0300

    Added sidebar menu

--- pag-chamado/chamado.js
+++ pag-chamado/chamado.js
@@ -1,7 +1,6 @@
-import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
-
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('chamado-form');
+    let ultimoId = localStorage.getItem('codigoChamado') ?? 0;
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -26,20 +25,45 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function salvarChamado(equipe, problema) {
-        var codigo = nanoid();
-        var tipo = document.getElementById("identificacao").value;
-        var transaction = db.transaction(["chamados"], "readwrite");
-        var objectStore = transaction.objectStore("chamados");
-        var request = objectStore.add({ codigo: codigo, descricao: problema, equipe: equipe, tipo: tipo, status: 'ativo' });
+        let codigo = gerarIdAutoIncrement();
+        localStorage.setItem('codigoChamado', codigo);
+        alert( localStorage.getItem('codigoChamado') ); 
+        let request = indexedDB.open("Tecsidel", 2);
 
+        request.onupgradeneeded = function(event) {
+            let db = event.target.result;
+            if (!db.objectStoreNames.contains("chamados")) {
+                db.createObjectStore("chamados", { keyPath: 'codigo' });
+            }
+        };
+        
         request.onsuccess = function(event) {
-            console.log("Chamado salvo com sucesso.");
-            form.reset();
-            exibirChamados();
+            let db = event.target.result;
+            let transaction = db.transaction(["chamados"], "readwrite");
+            let objectStore = transaction.objectStore("chamados");
+            let addRequest = objectStore.add({ codigo: codigo, descricao: problema, equipe: equipe, status: 'ativo' });
+
+            addRequest.onsuccess = function(event) {
+                console.log("Chamado salvo com sucesso.");
+                form.reset();
+                window.location.href = 'http://127.0.0.1:5500/pag-chamados/chamados.html';
+            };
+
+            addRequest.onerror = function(event) {
+                console.log(codigo);
+                console.log(event)
+                console.log("Erro ao salvar o chamado.");
+            };
         };
 
         request.onerror = function(event) {
-            console.log("Erro ao salvar o chamado.");
+            console.log("Erro ao abrir o banco de dados.");
         };
+    }
+
+    function gerarIdAutoIncrement()
+    {
+        ultimoId++;
+        return ultimoId;
     }
 });
--- pag-chamados/chamados.js
+++ pag-chamados/chamados.js
@@ -1,25 +1,40 @@
+var db; 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var request = indexedDB.open("Tecsidel", 2);
+
+    request.onsuccess = function(event) {
+        db = event.target.result;
+        exibirChamados(); 
+    };
+
+    request.onerror = function(event) {
+        console.log("Erro ao abrir o banco de dados.");
+    };
+});
+
 function exibirChamados() {
-  var transaction = db.transaction(["chamados"]);
-  var objectStore = transaction.objectStore("chamados");
-  var request = objectStore.openCursor();
+    var transaction = db.transaction(["chamados"]);
+    var objectStore = transaction.objectStore("chamados");
+    var request = objectStore.openCursor();
 
-  var chamadosBody = document.getElementById("chamados");
-  chamadosBody.innerHTML = '';
+    var chamadosBody = document.getElementById("chamados");
+    chamadosBody.innerHTML = '';
 
-  request.onsuccess = function(event) {
-      var cursor = event.target.result;
-      if (cursor) {
-          var chamado = cursor.value;
-          var row = chamadosBody.insertRow();
-          row.insertCell(0).textContent = chamado.codigo;
-          row.insertCell(1).textContent = chamado.descricao;
-          row.insertCell(2).textContent = chamado.tipo;
-          row.insertCell(3).textContent = chamado.status;
-          cursor.continue();
-      }
-  };
+    request.onsuccess = function(event) {
+        var cursor = event.target.result;
+        if (cursor) {
+            var chamado = cursor.value;
+            var row = chamadosBody.insertRow();
+            row.insertCell(0).textContent = chamado.codigo;
+            row.insertCell(1).textContent = chamado.descricao;
+            row.insertCell(2).textContent = chamado.equipe; 
+            row.insertCell(3).textContent = chamado.status;
+            cursor.continue();
+        }
+    };
 
-  request.onerror = function(event) {
-      console.log("Erro ao exibir os chamados.");
-  };
+    request.onerror = function(event) {
+        console.log("Erro ao exibir os chamados.");
+    };
 }
