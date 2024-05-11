48f9e5645b52445f869397bad925ef432fac50c2
pedroarthuralvesdeoliveira <pedroarthuralvesdeoliveira@hotmail.com>
2024-05-11 14:47:03 -0300

    Added validation in case of it does not have tickets

--- pag-chamado/chamado.js
+++ pag-chamado/chamado.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var parametros = new URLSearchParams(window.location.search);
-    var alerta = parametros.get("alerta");
-
+    const parametros = new URLSearchParams(window.location.search);
+    const alerta = parametros.get("alerta");
     const form = document.getElementById('chamado-form');
     const idAlerta = document.getElementById('id-alerta');
+
     if (alerta != null || alerta !== '') idAlerta.value = alerta; 
     let ultimoId = localStorage.getItem('codigoChamado') ?? 0;
 
@@ -18,37 +18,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function salvarChamado(equipe, problema) {
         let codigo = gerarIdAutoIncrement();
-        localStorage.setItem('codigoChamado', codigo);
-        let request = indexedDB.open("Tecsidel", 2);
-
-        request.onupgradeneeded = function(event) {
-            let db = event.target.result;
-            if (!db.objectStoreNames.contains("chamados")) {
-                db.createObjectStore("chamados", { keyPath: 'codigo' });
-            }
-        };
-        
-        request.onsuccess = function(event) {
-            let db = event.target.result;
-            let transaction = db.transaction(["chamados"], "readwrite");
-            let objectStore = transaction.objectStore("chamados");
-            let addRequest = objectStore.add({ codigo: codigo, descricao: problema, equipe: equipe, status: 'ativo' });
-
-            addRequest.onsuccess = function(event) {
-                console.log("Chamado salvo com sucesso.");
-                form.reset();
-                window.location.href = 'http://127.0.0.1:5500/pag-chamados/chamados.html';
-            };
-
-            addRequest.onerror = function(event) {
-                console.log("Erro ao salvar o chamado.");
-            };
-        };
-
-        request.onerror = function(event) {
-            console.log("Erro ao abrir o banco de dados.");
-        };
+        let chamados = JSON.parse(localStorage.getItem('chamados')) || []; 
+        chamados.push({ codigo: codigo, descricao: problema, equipe: equipe, status: 'ativo' }); 
+        localStorage.setItem('chamados', JSON.stringify(chamados)); 
+        form.reset();
+        window.location.href = 'http://127.0.0.1:5500/pag-chamados/chamados.html';
     }
+    
 
     function gerarIdAutoIncrement()
     {
--- pag-chamados/chamados.js
+++ pag-chamados/chamados.js
@@ -1,40 +1,20 @@
-var db; 
-
 document.addEventListener('DOMContentLoaded', function() {
-    var request = indexedDB.open("Tecsidel", 2);
-
-    request.onsuccess = function(event) {
-        db = event.target.result;
-        exibirChamados(); 
-    };
-
-    request.onerror = function(event) {
-        console.log("Erro ao abrir o banco de dados.");
-    };
+   exibirChamados();
 });
 
 function exibirChamados() {
-    var transaction = db.transaction(["chamados"]);
-    var objectStore = transaction.objectStore("chamados");
-    var request = objectStore.openCursor();
-
-    var chamadosBody = document.getElementById("chamados");
+    const chamadosBody = document.getElementById('chamados');
+    const chamados = JSON.parse(localStorage.getItem('chamados'));
     chamadosBody.innerHTML = '';
+    if (chamados !== null) chamados.map((chamado) => montarNaTela(chamado, chamadosBody));
+}   
 
-    request.onsuccess = function(event) {
-        var cursor = event.target.result;
-        if (cursor) {
-            var chamado = cursor.value;
-            var row = chamadosBody.insertRow();
-            row.insertCell(0).textContent = chamado.codigo;
-            row.insertCell(1).textContent = chamado.descricao;
-            row.insertCell(2).textContent = chamado.equipe; 
-            row.insertCell(3).textContent = chamado.status;
-            cursor.continue();
-        }
-    };
-
-    request.onerror = function(event) {
-        console.log("Erro ao exibir os chamados.");
-    };
+function montarNaTela(chamado, chamadosBody) {
+    const tr = document.createElement('tr');
+    for (const key in chamado) {
+        const td = document.createElement('td');
+        td.innerText = chamado[key];
+        tr.appendChild(td);
+    }
+    chamadosBody.appendChild(tr);
 }
